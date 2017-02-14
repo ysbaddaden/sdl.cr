96d384b03ce2603439476126b7259e1eef4c64d0
Michael Prins <michael@leap.ly>
2017-02-14 23:59:32 +0200

    Resolve duplicate SDL::Event::TouchFinger#touch_id

--- src/events.cr
+++ src/events.cr
@@ -157,7 +157,7 @@ module SDL
         @event.touchId
       end
 
-      def touch_id
+      def finger_id
         @event.fingerId
       end
     end
@@ -269,7 +269,7 @@ module SDL
     # queue is empty.
     def self.poll
       case LibSDL.poll_event(out event)
-      when 1 then
+      when 1
         from(event)
       when -1
         raise Error.new("SDL_PollEvent")
