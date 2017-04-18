7e80d45a0141195a6b160ae23d5f0b9f3eef5038
Jeremy Woertink <jeremywoertink@gmail.com>
2017-04-18 11:07:55 -0700

    update sample to play with new interface

--- samples/21_sound_and_effects.cr
+++ samples/21_sound_and_effects.cr
@@ -6,12 +6,16 @@ SDL.init(SDL::Init::VIDEO | SDL::Init::AUDIO); at_exit { SDL.quit }
 SDL::MIX.init(SDL::MIX::Init::MP3); at_exit { SDL::MIX.quit }
 SDL::MIX.open
 
-music = SDL::MIX.load_music(File.join(__DIR__, "data", "beat.wav"))
+DATA_DIR = File.join(__DIR__, "data")
 
-sounds = {} of String => LibMIX::Mix_Chunk*
-names = %w(high medium low scratch)
-names.each do |name|
-  sounds[name] = SDL::MIX.load_wav(File.join(__DIR__, "data", "#{name}.wav"))
+music = SDL::MIX::Music.new(File.join(DATA_DIR, "beat.wav"))
+
+samples = {} of String => SDL::MIX::Sample
+channels = {} of String => SDL::MIX::Channel
+
+%w(high medium low scratch).each_with_index do |name, idx|
+  samples[name] = SDL::MIX::Sample.new(File.join(DATA_DIR, "#{name}.wav"))
+  channels[name] = SDL::MIX::Channel.new(idx)
 end
 
 window = SDL::Window.new("SDL Tutorial", 640, 480)
@@ -22,35 +26,88 @@ activekey = [] of LibSDL::Keycode
 loop do
   case event = SDL::Event.wait
   when SDL::Event::Quit
-    #SDL::MIX.unload_music(music)
+    music.stop
+    SDL::MIX.close
     break
   when SDL::Event::Keyboard
     key = event.sym
     unless activekey.includes? key
       case key
       when .key_1?
-        SDL::MIX.play_wav(sounds["high"])
+        SDL::MIX::Channel.play(samples["high"]) # allocate any free channel
       when .key_2?
-        SDL::MIX.play_wav(sounds["medium"])
+        channels["medium"].play(samples["medium"]) # play through specific channel
       when .key_3?
-        SDL::MIX.play_wav(sounds["low"])
+        channels["low"].play(samples["low"])
       when .key_4?
-        SDL::MIX.play_wav(sounds["scratch"])
+        channels["scratch"].play(samples["scratch"])
       when .key_9?
-        if SDL::MIX.music_paused?
-          SDL::MIX.resume_playing_music
-        elsif SDL::MIX.music_playing?
-          SDL::MIX.pause_music
+        if music.paused?
+          music.resume
+        elsif music.playing?
+          music.pause
         else
-          SDL::MIX.play_music(music)
+          music.play
         end
       when .key_0?
-        SDL::MIX.stop_music
+        music.resume if music.paused?
+        music.stop
       end
       activekey << key
     end
     activekey.delete key if event.keyup?
   end
+
   png.blit(window.surface)
   window.update
 end
+
+
+#music = SDL::MIX.load_music(File.join(__DIR__, "data", "beat.wav"))
+#
+#sounds = {} of String => LibMIX::Mix_Chunk*
+#names = %w(high medium low scratch)
+#names.each do |name|
+#  sounds[name] = SDL::MIX.load_wav(File.join(__DIR__, "data", "#{name}.wav"))
+#end
+#
+#window = SDL::Window.new("SDL Tutorial", 640, 480)
+#png = SDL::IMG.load(File.join(__DIR__, "data", "prompt.png"))
+#png = png.convert(window.surface)
+#activekey = [] of LibSDL::Keycode
+#
+#loop do
+#  case event = SDL::Event.wait
+#  when SDL::Event::Quit
+#    #SDL::MIX.unload_music(music)
+#    break
+#  when SDL::Event::Keyboard
+#    key = event.sym
+#    unless activekey.includes? key
+#      case key
+#      when .key_1?
+#        SDL::MIX.play_wav(sounds["high"])
+#      when .key_2?
+#        SDL::MIX.play_wav(sounds["medium"])
+#      when .key_3?
+#        SDL::MIX.play_wav(sounds["low"])
+#      when .key_4?
+#        SDL::MIX.play_wav(sounds["scratch"])
+#      when .key_9?
+#        if SDL::MIX.music_paused?
+#          SDL::MIX.resume_playing_music
+#        elsif SDL::MIX.music_playing?
+#          SDL::MIX.pause_music
+#        else
+#          SDL::MIX.play_music(music)
+#        end
+#      when .key_0?
+#        SDL::MIX.stop_music
+#      end
+#      activekey << key
+#    end
+#    activekey.delete key if event.keyup?
+#  end
+#  png.blit(window.surface)
+#  window.update
+#end
