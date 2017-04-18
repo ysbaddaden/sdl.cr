a32b87e472095684818819bdca327d0bda85efd2
Jeremy Woertink <jeremywoertink@gmail.com>
2017-04-18 11:07:40 -0700

    add in Sample, Channel, and Music classes from @mjago

--- src/mix.cr
+++ src/mix.cr
@@ -4,7 +4,7 @@ require "./sdl"
 module SDL
   module MIX
     alias Init = LibMIX::Init
-    MAX_VOLUME = 128
+    MAX_VOLUME = LibMIX::MIN_MAX_VOLUME
     
     enum Type
       AIFF
@@ -37,86 +37,213 @@ module SDL
       ret
     end
 
-    # load long sound file
-    def self.load_music(filename, type : Type? = nil)
-      rwops = LibSDL.rw_from_file(filename, "rb")
-      if type
-        audio = LibMIX.load_mus_type_rw(rwops, type.to_s, 1)
-        raise SDL::Error.new("Mix_LoadMUSType_RW") unless audio
-      else
-        audio = LibMIX.load_mus_rw(rwops, 1)
-        raise SDL::Error.new("Mix_LoadMUS_RW") unless audio
+    def self.close
+      LibMIX.close_audio
+    end
+
+  end
+
+  module MIX
+    class Sample
+      protected getter sample
+
+      def self.decoder_count
+        LibMIX.get_num_chunk_decoders
       end
-      audio
-    end
 
-    # free long sound file
-    def self.unload_music(music)
-      LibMIX.free_music(music)
-    end
+      def self.decoder_name(index)
+        LibMIX.get_chunk_decoder(index)
+      end
 
-    # -1 is repeat until stopped
-    def self.play_music(music, repeats = -1)
-      LibMIX.play_music(music, repeats)
-    end
+      def initialize(filename = nil)
+        load filename if filename
+      end
 
-    def self.pause_music
-      LibMIX.pause_music
-    end
+      def load(filename)
+        rwops = LibSDL.rw_from_file(filename, "rb")
+        @sample = LibMIX.load_wav_rw(rwops, 1)
+        raise SDL::Error.new("Mix_LoadWAV_RW") unless @sample
+      end
 
-    def self.stop_music
-      LibMIX.halt_music
+      def finalize
+        LibMIX.free_chunk(sample)
+      end
     end
+  end
 
-    def self.music_paused?
-      LibMIX.music_paused == 1
+  module MIX
+    class Music
+      private getter music : Pointer(LibMIX::Music) | Nil
+
+      def initialize(filename = nil)
+        @music = filename ? load(filename) : nil
+      end
+
+      def play(repeats = -1)
+        LibMIX.play_music(music, repeats)
+      end
+
+      def pause
+        LibMIX.pause_music
+      end
+
+      def resume
+        LibMIX.resume_music
+      end
+
+      def stop
+        LibMIX.halt_music
+      end
+
+      def playing?
+        LibMIX.music_playing == 1
+      end
+
+      def paused?
+        LibMIX.music_paused == 1
+      end
+
+      def rewind
+        LibMIX.rewind_music
+      end
+
+      def fade_in(loops = -1, msec = 1000)
+        LibMIX.fade_in_music(music, loops, msec)
+      end
+
+      def fade_out(msec = 1000)
+        LibMIX.fade_out_music(msec)
+      end
+
+      def volume=(volume)
+        LibMIX.music_volume(volume > MAX_VOLUME ? MAX_VOLUME : volume)
+      end
+
+      def volume
+        LibMIX.music_volume(-1)
+      end
+
+      def load(filename, typ : Type? = nil)
+        rwops = LibSDL.rw_from_file(filename, "rb")
+        @music = typ ? load_music_type(rwops, typ) : load_music(rwops)
+      end
+
+      def finalize
+        LibMIX.free_music(music)
+        @music = nil
+      end
+
+      private def load_music(rwops)
+        audio = LibMIX.load_mus_rw(rwops, 1)
+        raise SDL::Error.new("Mix_LoadMUSType_RW") unless audio
+        audio
+      end
+
+      private def load_music_type(rwops, typ)
+        audio = LibMIX.load_mus_type_rw(rwops, typ.to_s, 1)
+        raise SDL::Error.new("Mix_LoadMUS_RW") unless audio
+        audio
+      end
     end
+  end
 
-    def self.music_playing?
-      LibMIX.music_playing == 1
-    end
+  module MIX
+    class Channel
+      property id
 
-    def self.resume_playing_music
-      LibMIX.resume_music
-    end
+      def initialize(@id = 1)
+        @@count ||= 8 unless @@count
+      end
 
-    def self.rewind_music
-      LibMIX.rewind_music
-    end
+      def self.allocate_channels(count)
+        @@count = count
+        LibMIX.allocate_channels(count)
+      end
 
-    def self.fade_in_music(music, loops = -1, ms = 1000)
-      LibMIX.fade_in_music(music, loops, ms)
-    end
+      def self.reserve_channels(count)
+        @@reserved = count
+        LibMIX.reserve_channels(count)
+      end
 
-    def self.fade_out_music(ms = 1000)
-      LibMIX.fade_out_music(ms)
-    end
+      def self.play(smpl, repeats = 0)
+        LibMIX.play_channel(-1, smpl.sample, repeats)
+      end
 
-    def self.music_volume=(volume)
-      LibMIX.music_volume(volume > MAX_VOLUME ? MAX_VOLUME : volume)
-    end
+      def self.play(smpl, repeats = 0, ticks = -1)
+        LibMIX.play_channel_timed(-1, smpl.sample, repeats, ticks)
+      end
 
-    def self.music_volume()
-      LibMIX.music_volume(-1)
-    end
+      def self.fade_in(smpl : Sample, loops = 0, ms = 1000, ticks = -1)
+        LibMIX.fade_in_channel(-1, smpl.sample, loops, ms, ticks)
+      end
 
-    # load short sound file
-    def self.load_wav(filename)
-      rwops = LibSDL.rw_from_file(filename, "rb")
-      audio = LibMIX.load_wav_rw(rwops, 1)
-      raise SDL::Error.new("Mix_LoadWAV_RW") unless audio
-      audio
-    end
+      def self.fade_out(ms = 1000)
+        LibMIX.fade_out_channel(-1, ms)
+      end
 
-    # free short sound file
-    def self.unload_wav(sound)
-      LibMIX.free_chunk(sound)
-    end
+      def self.resume
+        LibMIX.resume_music(-1)
+      end
 
-    # -1 for channel is the nearest channel.
-    # 0 repeats is play once
-    def self.play_wav(sound, channel = -1, repeats = 0, ticks = -1)
-      LibMIX.play_channel_timed(channel, sound, repeats, ticks)
+      def self.expire(ticks)
+        LibMIX.channel_expire(-1, ticks)
+      end
+
+      def self.channels
+        @@count || 0
+      end
+
+      def self.reserved
+        @@reserved || 0
+      end
+
+      def self.volume=(volume)
+        LibMIX.channel_volume(-1, volume > MAX_VOLUME ? MAX_VOLUME : volume)
+      end
+
+      def self.paused_count
+        LibMIX.channel_paused(-1)
+      end
+
+      def self.finished(func)
+        LibMIX.cb_channel_finished(func)
+      end
+
+      def play(smpl, repeats = 0)
+        LibMIX.play_channel(id, smpl.sample, repeats)
+      end
+
+      def play(smpl, repeats = 0, ticks = -1)
+        LibMIX.play_channel_timed(id, smpl.sample, repeats, ticks)
+      end
+
+      def fade_in(smpl : Sample, loops = 0, ms = 1000, ticks = -1)
+        LibMIX.fade_in_channel(id, smpl.sample, loops, ms, ticks)
+      end
+
+      def fade_out(ms = 1000)
+        LibMIX.fade_out_channel(id, ms)
+      end
+
+      def expire
+        LibMIX.channel_expire(id, ticks)
+      end
+
+      def fading?
+        LibMIX.fading? id
+      end
+
+      def paused?
+        LibMIX.channel_paused(id) == 1
+      end
+
+      def volume=(volume)
+        LibMIX.channel_volume(id, volume > MAX_VOLUME ? MAX_VOLUME : volume)
+      end
+
+      def volume
+        LibMIX.channel_volume(id, -1)
+      end
     end
   end
 end
