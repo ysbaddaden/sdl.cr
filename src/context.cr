01a75e3f5cb48048f8328adb3fd82990a7a5e871
Nils Reid <nils.reid@neoshadow.co>
2017-08-11 17:16:40 +0200

    Refractoring a bit, removed most of the type definitions and the returns. Formated the file with Crystal.

--- src/context.cr
+++ src/context.cr
@@ -1,118 +1,98 @@
-
 require "./window.cr"
 
 module SDL
+  class GLContext
+    alias Attribute = LibSDL::GLattr
+    alias Profile = LibSDL::GLprofile
+    alias Flag = LibSDL::GLcontextFlag
 
-	class GLContext
-	
-		alias Attribute = LibSDL::GLattr
-		alias Profile = LibSDL::GLprofile
-		alias Flag = LibSDL::GLcontextFlag
+    @@library_loaded = false
 
-		@@lib_loaded = false
+    def self.load_library(path)
+      return if @@library_loaded
 
+      ret = 0
 
-		def self.load_library( path : String | Nil ) : Nil
-			return if @@lib_loaded
+      ret = LibSDL.gl_load_library(path)
 
-			ret = 0
+      raise Error.new("SDL_GL_LoadLibrary") unless ret == 0
+      @@library_loaded = true
+      nil
+    end
 
-			if path.nil?
-				ret = LibSDL.gl_load_library( Pointer.null )
-			else
-				ret = LibSDL.gl_load_library( path )
-			end
+    def self.unload_library
+      # Maybe raise an error ?
+      return unless @@library_loaded
 
-			raise Error.new( "SDL_GL_LoadLibrary" ) unless ret == 0
-			@@lib_loaded = true
-		end
+      LibSDL.gl_unload_library
+      @@library_loaded = false
+      nil
+    end
 
-		def self.unload_library : Nil
-			return unless @@lib_loaded
+    # TODO: Maybe a generic function returning a `Proc` object ?
+    # not good enough for doing it right.
+    def self.get_proc_address(name)
+      raise Error.new("OpenGL Library not loaded") unless @@library_loaded
 
-			LibSDL.gl_unload_library
-			@@lib_loaded = false
-		end
+      LibSDL.gl_get_proc_address(name)
+    end
 
-		# TODO: Maybe a generic function returning a `Proc` object ?
-		# not good enough for doing it right.
-		def self.get_proc_address( name : String ) : Void*
-			raise Error.new( "OpenGL Library not loaded" ) unless @@lib_loaded
+    def self.get_attribute(attribute)
+      val = 0
+      ret = LibSDL.gl_get_attribute(attribute, pointerof(val))
+      raise Error.new("SDL_GL_GetAttribute") unless ret == 0
 
-			LibSDL.gl_get_proc_address( name )
-		end
+      val
+    end
 
+    def self.set_attribute(attribute, value)
+      ret = LibSDL.gl_set_attribute(attribute, value)
+      raise Error.new("SDL_GL_SetAttribute") unless ret == 0
+    end
 
+    def self.reset_attributes
+      LibSDL.get_reset_attributes
+    end
 
-		def self.get_attribute( attr : Attribute ) : Int
-			val = 0
-			ret = LibSDL.gl_get_attribute( attr, pointerof(val) )
-			raise Error.new( "SDL_GL_GetAttribute" ) unless ret == 0
+    def self.is_extension_supported?(name)
+      LibSDL.gl_extension_supported(name)
+    end
 
-			return val
-		end
+    def self.drawable_size(window)
+      LibSDL.gl_get_drawable_size(window, out w, out h)
+      {w, h}
+    end
 
-		def self.set_attribute( attr : Attribute, val : Int ) : Nil
-			ret = LibSDL.gl_set_attribute( attr, val )
-			raise Error.new("SDL_GL_SetAttribute" ) unless ret == 0
-		end
+    def self.swap_window(window) : Nil
+      LibSDL.gl_swap_window(w)
+    end
 
-		def self.reset_attributes : Nil
-			LibSDL.get_reset_attributes
-		end
+    def self.swap_interval
+      LibSDL.gl_get_swap_interval
+    end
 
+    def self.swap_interval=(interval)
+      ret = LibSDL.gl_set_swap_interval(interval)
+      raise Error.new("SDL_GL_SetSwapInterval") unless ret == 0
 
+      interval
+    end
 
-		def self.is_extension_supported?( name : String ) : Bool
-			return LibSDL.gl_extension_supported( name )
-		end
+    def initialize(window)
+      @context = LibSDL.gl_create_context(window)
+    end
 
+    def finalize
+      LibSDL.gl_destroy_context(@context)
+    end
 
+    def make_current(window)
+      ret = LibSDL.gl_make_current(window, @context)
+      raise Error.new("SDL_GL_MakeCurrent") unless ret == 0
+    end
 
-		def self.drawable_size( w : Window )
-			LibSDL.gl_get_drawable_size( w, out w, out h )
-			return { w, h }
-		end
-
-
-		def self.swap_window( w : Window ) : Nil
-			LibSDL.gl_swap_window( w )
-		end
-
-
-		def self.get_swap_interval : Int
-			return LibSDL.gl_get_swap_interval
-		end
-
-		def self.set_swap_interval( nv : Int ) : Nil
-			ret = LibSDL.gl_set_swap_interval( nv )
-			raise Error.new( "SDL_GL_SetSwapInterval" ) unless ret == 0
-		end
-
-
-
-		def initialize( window : Window )
-			@context = LibSDL.gl_create_context( window )
-		end
-
-		def finalize
-			LibSDL.gl_destroy_context( @context )
-		end
-
-
-
-		def make_current( w : Window ) : Nil
-			ret = LibSDL.gl_make_current( w, @context )
-			raise Error.new( "SDL_GL_MakeCurrent" ) unless ret == 0
-		end
-
-
-		def to_unsafe
-			@context
-		end
-
-	end
-
+    def to_unsafe
+      @context
+    end
+  end
 end
-
-
