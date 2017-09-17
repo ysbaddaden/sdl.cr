77edd7bd7e90ad812a29bb2839e506c54ee012be
Jeremy Woertink <jeremywoertink@gmail.com>
2017-09-17 16:41:29 -0700

    adding in the rest of the timer methods

--- src/timer.cr
+++ src/timer.cr
@@ -5,8 +5,24 @@ module SDL
       LibSDL.add_timer(interval, callback)
     end
 
+    def self.delay(ms : UInt32)
+      LibSDL.delay(ms)
+    end
+
+    def self.performance_counter
+      LibSDL.get_performance_counter
+    end
+
+    def self.performance_frequency
+      LibSDL.get_performance_frequency
+    end
+
     def self.ticks
       LibSDL.get_ticks
+    end
+
+    def self.remove(timer_id)
+      LibSDL.remove_timer(timer_id)
     end
   end
 end
