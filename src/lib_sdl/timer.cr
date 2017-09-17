13a2c756d128d5d419b628d363d42bd931bc5a45
Jeremy Woertink <jeremywoertink@gmail.com>
2017-09-17 16:40:32 -0700

    fixing typo

--- src/lib_sdl/timer.cr
+++ src/lib_sdl/timer.cr
@@ -6,7 +6,7 @@ lib LibSDL
   fun add_timer = SDL_AddTimer(interval : UInt32, callback : TimerCallback, param : Void*) : TimerID
   fun delay = SDL_Delay(ms : UInt32) : Void
   fun get_performance_counter = SDL_GetPerformanceCounter() : UInt64
-  fun get_performance_frequence = SDL_GetPerformanceFrequency() : UInt64
+  fun get_performance_frequency = SDL_GetPerformanceFrequency() : UInt64
   fun get_ticks = SDL_GetTicks() : UInt32
   fun remove_timer = SDL_RemoveTimer(id : TimerID) : Bool
   fun ticks_passed = SDL_TICKS_PASSED(a : UInt32, b : UInt32) : Bool
