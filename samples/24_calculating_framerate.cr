efa4ae7adc847f6ccb1fca78396214f59fb95404
Bradyn Glines <glinesbdev@gmail.com>
2022-11-06 01:11:39 -0400

    Added framerate samples

---
+++ samples/24_calculating_framerate.cr
@@ -0,0 +1,46 @@
+require "string_pool"
+
+require "../src/sdl"
+require "../src/ttf"
+
+SDL.init(SDL::Init::VIDEO); at_exit { SDL.quit }
+SDL::TTF.init; at_exit { SDL::TTF.quit }
+SDL.set_hint(SDL::Hint::RENDER_SCALE_QUALITY, "1")
+
+window = SDL::Window.new("SDL tutorial", 640, 480)
+renderer = SDL::Renderer.new(window, SDL::Renderer::Flags::ACCELERATED | SDL::Renderer::Flags::PRESENTVSYNC)
+font = SDL::TTF::Font.new(File.join(__DIR__, "data", "lazy.ttf"), 28)
+
+pool = StringPool.new
+fps_text = pool.get("Average Frames Per Second: ")
+font_color = SDL::Color[0]
+
+start_ticks = Time.monotonic.total_milliseconds
+frames = 0
+
+
+loop do
+  cap_ticks = Time.monotonic.total_milliseconds
+
+  case event = SDL::Event.poll
+  when SDL::Event::Quit
+    break
+  end
+
+  average_fps = frames / ((Time.monotonic.total_milliseconds - start_ticks) / 1000)
+  average_fps = 0 if average_fps > 2_000_000
+
+  rendered_text = "#{fps_text}#{average_fps.format(decimal_places: 2)}"
+  surface = font.render_shaded(rendered_text, font_color, renderer.draw_color)
+
+  renderer.draw_color = SDL::Color[255]
+  renderer.clear
+
+  x = (window.width - surface.width) // 2
+  y = (window.height - surface.height) // 2
+
+  renderer.copy(surface, dstrect: SDL::Rect[x, y, surface.width, surface.height])
+  renderer.present
+
+  frames += 1
+end
