5a222701cc83aad89e1eab80812af97b12d6188a
Julien Portalier <julien@portalier.com>
2025-06-29 15:30:26 +0200

    Fix: compilation of samples

--- samples/14_animated_sprites_and_vsync.cr
+++ samples/14_animated_sprites_and_vsync.cr
@@ -25,9 +25,9 @@ loop do
   renderer.draw_color = SDL::Color[255, 255, 255, 255]
   renderer.clear
 
-  current_clip = sprite_clips[frame / slowdown]
-  x = (window.width - current_clip.w) / 2
-  y = (window.height - current_clip.h) / 2
+  current_clip = sprite_clips[frame // slowdown]
+  x = (window.width - current_clip.w) // 2
+  y = (window.height - current_clip.h) // 2
   renderer.copy(sprite, current_clip, SDL::Rect[x, y, current_clip.w, current_clip.h])
 
   renderer.present
